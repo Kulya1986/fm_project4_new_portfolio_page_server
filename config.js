edeb3185c3f52110482d7d0b5f0f8ef128b64456
Kulya1986 <87373027+Kulya1986@users.noreply.github.com>
2023-11-21 18:14:58 +0200

    Sending emails via gmail

---
+++ config.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//     clientId : '43339957105-2jtmln2eocrdm3hj179bsn7l66k4gr54.apps.googleusercontent.com',
+//     clientSecret : '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'
+// }
+
+export const configData = {
+    user: 'kulychka@gmail.com',
+    clientId : '43339957105-2jtmln2eocrdm3hj179bsn7l66k4gr54.apps.googleusercontent.com',
+    clientSecret : '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',
+    refreshToken : '1//04uBiOdCUF2i3CgYIARAAGAQSNwF-L9Ir0cDbNCazUM3Q8frcRpNullKLTE--jL61xBymJqPlFYfmeupzbGxF7tHPKsQSa7N6qgw'
+}
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
+import nodemailer from 'nodemailer';
+import  { google } from 'googleapis';
+import { configData } from '../new_portfolio_page_server/config.js';
+
+const OAuth2 = google.auth.OAuth2;
+const OAuth2_client = new OAuth2(configData.clientId, configData.clientSecret);
+OAuth2_client.setCredentials({ refresh_token : configData.refreshToken});
+
 
 const app = express();
 app.use(express.json()); //middleware to parse JSON format of the frontend, from latest versions built-in the express library, no need to import bodyParser library
@@ -10,6 +18,7 @@ app.use(cors());
 app.post('/send-msg', (req, res) => {
     const {senderName, senderEmail, senderMsg} = req.body;
     const strToWrite = senderName+';\t'+senderEmail+';\t'+senderMsg+'\n';
+    //writing message info to file
     fs.writeFile('messages.txt', strToWrite, {'flag':'a'}, 
         (err) => { 
             if (err)
@@ -21,6 +30,36 @@ app.post('/send-msg', (req, res) => {
                 res.status(200).json('message sent');
             }
             });
+    //sending email with message
+    const accessToken = OAuth2_client.getAccessToken();
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: 'OAuth2',
+            user: configData.user,
+            clientId: configData.clientId,
+            clientSecret: configData.clientSecret,
+            refreshToken: configData.refreshToken,
+            accessToken: accessToken
+        }
+    })
+
+    const mailOptions = {
+        from: 'Portfolio page <${configData.user}>',
+        to: 'kulychka@ukr.net',
+        subject: 'Portfolio page - from '+ senderName + '<'+senderEmail+'>',
+        text: senderMsg
+    }
+
+    transporter.sendMail(mailOptions, function(error, result){
+        if (error){
+            console.log ('Error: ' , error)
+        }else{
+            console.log('Success: ', result)
+        }
+        transporter.close();  
+    })
 })
 
 app.listen(3000, ()=>{
